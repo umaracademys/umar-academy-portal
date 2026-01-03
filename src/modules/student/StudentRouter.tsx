92fd6df5bbf7b822e4620083ce6c6d307abcf18e
umaracademys <umaracademys@gmail.com>
2026-01-03 10:26:49 -0600

    Update student and teacher portals to use new professional messaging UI
    - Replace old MessagesPage with ProfessionalMessagesPage for all users
    - Update SuperAdminMessagesPage to use new component
    - Add ProfessionalMessagesPage to StudentRouter for /student/messages route
    - All users now get the institutional card-based messaging interface

--- src/modules/student/StudentRouter.tsx
+++ src/modules/student/StudentRouter.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import StudentDashboard from './pages/StudentDashboard';
 import StudentProfile from './pages/StudentProfile';
 import StudentCourses from './pages/StudentCourses';
 import StudentAssignments from './pages/StudentAssignments';
 import StudentPdfHomework from '../../components/StudentPdfHomework';
+
+const ProfessionalMessagesPage = lazy(() => import('../../components/messaging/ProfessionalMessagesPage'));
 
 const StudentRouter: React.FC = () => {
   console.log('🔍 StudentRouter - Component is rendering!');
@@ -18,7 +20,7 @@ const StudentRouter: React.FC = () => {
       <Route path="pdf-homework" element={<StudentPdfHomework />} />
       <Route path="progress" element={<div>Student Progress Page - Coming Soon</div>} />
       <Route path="payments" element={<div>Student Payments Page - Coming Soon</div>} />
-      <Route path="messages" element={<div>Student Messages Page - Coming Soon</div>} />
+      <Route path="messages" element={<ProfessionalMessagesPage />} />
       <Route path="" element={<StudentDashboard />} />
     </Routes>
   );
